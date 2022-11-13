9fbfc824f6358bcd5b19c84597e493e4ebae9c01
Metalderly <osvaldoeverdiaz@gmail.com>
2022-11-13 09:46:31 -0300

    remplazando nombre de tablas

--- base-de-datos/Proyecto1_BD.sql
+++ base-de-datos/Proyecto1_BD.sql
@@ -1,6 +1,6 @@
-create database Proyecto_BD;
+create database Proyecto1_BD;
 
-use Proyecto_BD;
+use Proyecto1_BD;
 
 create table Suscripcion (
 suscri_id int,
@@ -11,7 +11,7 @@ constraint pk_su primary key (suscri_id)
 
 select * from Suscripcion;
 
-create table Perfil1 (
+create table MiPerfil (
 per_1 int auto_increment,
 nombre varchar (20),
 apellido varchar (20),
@@ -32,7 +32,7 @@ constraint pk_sp primary key (per_1),
 constraint fk_fus foreign key (usuario1) references Suscripcion (suscri_id)  
 );
 
-create table Perfil2 (
+create table MiBanda (
 per_2 int auto_increment,
 integrantes varchar (200),
 nombreBanda varchar (50),
@@ -42,7 +42,7 @@ apodo varchar (20),
 letras varchar (50),
 per_1_1 int,
 constraint pk_per primary key (per_2),
-constraint fk_fp1_1 foreign key (per_1_1) references Perfil1 (per_1)
+constraint fk_fp1_1 foreign key (per_1_1) references MiPerfil (per_1)
 );
 
 create table MisVideos (
@@ -56,7 +56,7 @@ mis_Arpegios varchar (100),
 Mis_Melodias varchar (100),
 vid_1_1 int,
 constraint pk_v1 primary key (vid_1),
-constraint fk_fv1_1 foreign key (vid_1_1) references Perfil2 (per_2)
+constraint fk_fv1_1 foreign key (vid_1_1) references MiBanda (per_2)
 );
 
 create table mp3 (
@@ -68,8 +68,8 @@ misMelodias varchar (100),
 per_2_3 int,
 per_1_3 int,
 constraint pk_m primary key (mp_3),
-constraint fk_p_2_3 foreign key (per_2_3) references Perfil2 (per_2),
-constraint fk_p_1_3 foreign key (per_1_3) references Perfil1 (per_1)
+constraint fk_p_2_3 foreign key (per_2_3) references MiBanda (per_2),
+constraint fk_p_1_3 foreign key (per_1_3) references MiPerfil (per_1)
 );
 
 select * from mp3;
@@ -79,16 +79,18 @@ select * from mp3;
 create table Per1_Per2 (
 per_1_2 int,
 per_2_1 int,
-constraint fk_p_1_2 foreign key (per_1_2) references Perfil1 (per_1),
-constraint fk_p_2_1 foreign key (per_2_1) references Perfil2 (per_2)
+constraint fk_p_1_2 foreign key (per_1_2) references MiPerfil (per_1),
+constraint fk_p_2_1 foreign key (per_2_1) references MiBanda (per_2)
 );
+
+select * from Per1_Per2;
 
 -- Relacion Perfil2-MisVideos
 
 create table per2_Videos (
 per_2_2 int,
 vid_1_1 int,
-constraint fk_p_2_2 foreign key (per_2_2) references Perfil2 (per_2),
+constraint fk_p_2_2 foreign key (per_2_2) references MiBanda (per_2),
 constraint fk_v_1_1 foreign key (vid_1_1) references MisVideos (vid_1)
 );
 
@@ -98,7 +100,7 @@ create table Videos_Perfil1 (
 vid_1_2 int,
 per_1_4 int,
 constraint fk_v_1_2 foreign key (vid_1_2) references MisVideos (vid_1),
-constraint fk_p_1_4 foreign key (per_1_4) references Perfil1 (per_1)
+constraint fk_p_1_4 foreign key (per_1_4) references MiPerfil (per_1)
 );
 
 -- relacion MisMp3-Perfil1
@@ -107,7 +109,7 @@ create table MisMp3_Perfil1 (
 mp_3_2 int,
 Per_1_5 int,
 constraint fk_m_3_2 foreign key (mp_3_2) references mp3 (mp_3),
-constraint fk_p_1_5 foreign key (per_1_5) references Perfil1 (per_1)
+constraint fk_p_1_5 foreign key (per_1_5) references MiPerfil (per_1)
 );
 
 -- relacion MisMp3-Perfil2
@@ -116,5 +118,5 @@ create table Mp3_Perfil2 (
 mp_3_1 int,
 per_2_4 int,
 constraint fk_m_3_1 foreign key (mp_3_1) references mp3 (mp_3),
-constraint fk_p_2_4 foreign key (per_2_4) references Perfil2 (per_2)
+constraint fk_p_2_4 foreign key (per_2_4) references MiBanda (per_2)
 )
